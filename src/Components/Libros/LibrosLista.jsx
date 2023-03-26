5ca54b98779f112c0f5076bf43bd755c0d5498ab
Re-Start <nachoysabrigithub@gmail.com>
2023-03-26 19:43:15 -0300

    Dallape-EntregaFinal

--- src/Components/Libros/LibrosLista.jsx
+++ src/Components/Libros/LibrosLista.jsx
@@ -24,67 +24,22 @@ const LibrosLista = () => {
 
     
   return (
-    <>
+    <ul style={{ margin: 0, padding: '3rem' }}>
+    <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around', padding: '2rem', gap: '3rem' }} className='backgroundBooks'>
+      {data.map((item, index) => (
+        <React.Fragment key={index}>
+          <img src={'https://i.pinimg.com/564x/3b/5c/31/3b5c314ae9f7ba99b20b451bf2aeb81b.jpg'} alt={`Imagen ${index}`} style={{ width: '200px', height: 'auto', marginRight: '2rem', borderRadius: '0.5rem', gap: '1rem' }} />
+          <div style={{ gap: '2rem', width: '10rem', display: 'flex', justifyContent: 'center', flexDirection: 'column' }} className='nameBooks'>
+            <h6 style={{ fontFamily: 'Cinzel Decorative', fontWeight: 'bold', width: '5rem', display: 'flex', justifyContent: 'flex-start' }}>{item.name.toUpperCase()}</h6>
+            <h6 style={{ fontFamily: 'Cinzel Decorative', fontWeight: 'bold' }}>{item.price} </h6>
+            <button onClick={() => addToCart(item, 1)} style={{ borderRadius: '1rem', backgroundColor: '#5C2B29', border: '2px solid #8B0000', color: 'yellow' }}> Agregar al carrito</button>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  </ul>
+  
       
-      {/* <ul>
-        {
-          cart.map((item) => (
-            <li>
-              <span>
-                  {item.name} - {item.quantity} x {item.price} 
-              </span>
-            </li>
-          ))
-        }
-      </ul> */}
-      
-
-      <ul style={{ margin: 0, padding: '3rem' }}>
-        {api.length > 0 && (
-          <>
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap:'wrap', justifyContent:'space-evenly', padding:'2rem', gap:'3rem'}} className='backgroundBooks'>
-              {data.map((item, index) => {
-                const thumbnail = item?.volumeInfo?.imageLinks?.thumbnail;
-                return (
-                  <div key={item.id} style={{ display: 'flex', justifyContent: 'center', marginBottom: '1rem',flexWrap:'wrap' }} className='aumentoImg'>
-                    <div style={{paddingBottom:'2rem', paddingLeft:'3rem'}} >
-                      {thumbnail ? (
-                        <>
-                        <img src={thumbnail} alt={`Imagen ${index}`} style={{ width: '150px', height: 'auto', marginRight: '2rem',borderRadius:'0.5rem', gap:'1rem'}} />
-                        <li to={`/api/${index}`} style={{ gap: '2rem', width:'10rem', display:'flex',justifyContent:'center', flexDirection:'column' }} className='nameBooks'>
-
-                          <h6 style={{fontFamily: 'Cinzel Decorative', fontWeight:'bold',width:'5rem',display:'flex', justifyContent:'flex-start'}}>{json[index].name.toUpperCase()}</h6>
-                          <h6 style={{fontFamily: 'Cinzel Decorative', fontWeight:'bold'}}>{json[index].price} </h6>
-                          
-                          <button onClick={() => addToCart(item,1)} style={{borderRadius:'1rem'}}> Agregar al carrito</button>
-
-                        </li>
-                        
-                        </>
-                      ) : (
-                        <>
-                        <img src={'https://i.pinimg.com/564x/ab/db/2c/abdb2cbc5ccf686576cd9a4a1b1bf8dc.jpg  '} alt={`Imagen ${index}`} style={{ width: '150px', height: 'auto', marginRight: '2rem',borderRadius:'0.5rem', gap:'1rem'}} />
-                        <li to={`/api/${index}`} style={{ gap: '2rem', width:'10rem', display:'flex',justifyContent:'center', flexDirection:'column' }} className='nameBooks'>
-
-                          <h6 style={{fontFamily: 'Cinzel Decorative', fontWeight:'bold',width:'5rem',display:'flex', justifyContent:'flex-start'}}>{json[index].name.toUpperCase()}</h6>
-                          <h6 style={{fontFamily: 'Cinzel Decorative', fontWeight:'bold'}}>{json[index].price} </h6>
-                          <button onClick={() => addToCart(item,1)} style={{borderRadius:'1rem',backgroundColor:'#5C2B29',border: '2px solid #8B0000', color:'yellow'}}> Agregar al carrito</button>
-                          
-                        </li>
-                        </>
-                      )}
-                      
-                      
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </>
-        )}
-      </ul>
-      
-    </>
   )
 }
 
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -17,86 +17,94 @@ const NavBar = () => {
     }
 
     return (
-        <nav style={{display:'flex', justifyContent:'center'}} className='backgroundImg'>
+        <nav style={{display:'flex', justifyContent:'center', overflowX:'hidden'}} className='backgroundImg'>
             
             
-                <nav className="navbar navbar-expand-lg navbar-light container-fluid " style={{display:'flex', justifyContent:'center'}} >
-                    <div className="navbar-collapse" style={{display:'flex', justifyContent:'flex-start'}}>
-                        <div style={{ display:'flex',justifyContent:'flex-start'}}>
-                            <Link to="/">
-                                <img src="https://i.pinimg.com/564x/a4/58/18/a45818319b8af134a917e664c08bff92.jpg" alt=""  className='aumentoImg' style={{width:'8rem'}}/>
-                            </Link>
-                        </div>
-                        <div >
-                            <div style={{padding:'1rem', paddingLeft:'20rem',display:'flex', justifyContent:'center', gap:'8rem ', maxWidth:'minContent'}}>
-
-                                <div  className='aumentoImg' style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}>
-                                    
-                                    <Link to={'/'} className='button ' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Inicio </Link>
-                                </div>
-                                <div className="nav-item active aumentoImg" style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}  >
-                                    <Link to={'/LibrosLista'} className='button' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Libros </Link>
-                                </div>
-                                <div className="nav-item aumentoImg"  style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}>
-                                    <Link to={'/Nosotros'} className='button' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Sobre Nosotros </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </nav>
-            
-            
-                <div  className="cart-dropdown-wrapper aumentoImg" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
-                
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-cart" viewBox="0 0 16 16" style={{backgroundColor:'black', borderRadius:'0.2rem'}}>
-                            <path   d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                        
-                        </svg>
-                        
-                        <div className='flexEnd' style={{position:'relative', right:'2rem', top:'2rem', display:'flex', flexDirection:'row-reverse',width:'20rem'}}>
-            
-            <div style={{position:'relative', bottom:'2rem', right:'2rem', paddingTop:'1rem'}}>
-            { openCart &&  
-                <div className='cart-dropdown' style={{width:'370px', backgroundColor:'#5C2B29', borderRadius:'1rem'}}>
-                    <span  onClick={clearCart} style={{paddingBottom:'15px'}}> <button style={{ borderRadius:'1rem', backgroundColor:'black', color:'yellow', border:'1px white solid'}}>  Vaciar carrito </button> </span>
-
-                {
-
-                cart.map((item) => (
-                    <div style={{color:'white'}}>
-                    {item.name} - {item.quantity} Unidades - Precio individual: {item.price}
-                    <br />
-                    -----------------------------------------
-                    <br />
+        <div className="navbar navbar-expand-lg navbar-light container-fluid " style={{display:'flex', justifyContent:'center'}} >
+            <div className="navbar-collapse" style={{display:'flex', justifyContent:'flex-start'}}>
+                <div style={{ display:'flex',justifyContent:'flex-start'}}>
+                    <Link to="/">
+                        <img src="https://i.pinimg.com/564x/a4/58/18/a45818319b8af134a917e664c08bff92.jpg" alt=""  className='aumentoImg' style={{width:'8rem'}}/>
+                    </Link>
                 </div>
-                ))}
-                    
-                    
-                    <div>
-                        <br />
-                        <span style={{color:'white'}}>Cantidad de Libros: {getTotalQuantity()}  </span>
-                        <br />
-                        <span style={{color:'white'}}>Precio Total: $ {getTotal()} </span>
-                        <br />
-                        <span> <button style={{ borderRadius:'1rem', backgroundColor:'black', color:'yellow', border:'1px white solid'}}> Ir a Pagar </button></span>
-                        
-                    </div>
-                    
-                    
-            </div>
-            }
-            
-            </div>
-                </div>
-                
-                
-            </div>
+                <div >
+                    <div style={{padding:'1rem', paddingLeft:'20rem',display:'flex', justifyContent:'center', gap:'8rem ', maxWidth:'minContent'}}>
 
+                        <div  className='aumentoImg' style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}>
                             
-        </nav>
+                            <Link to={'/'} className='button ' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Inicio </Link>
+                        </div>
+                        <div className="nav-item active aumentoImg" style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}  >
+                            <Link to={'/LibrosLista'} className='button' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Libros </Link>
+                        </div>
+                        <div className="nav-item aumentoImg"  style={{backgroundColor:'#5C2B29', borderRadius:'0.5rem', border:'1px #ab9d35 solid'}}>
+                            <Link to={'/Nosotros'} className='button' style={{fontFamily: 'Cinzel Decorative', borderRadius:'0.5rem'}}>Sobre Nosotros </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    
+    
+        <div   onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{display:'flex',flexDirection:'column',justifyContent:'center',position:'absolute', right:'1rem', top:'2rem', width:'18rem'}}>
+        
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-cart" viewBox="0 0 16 16" style={{backgroundColor:'black', borderRadius:'0.2rem', position:'relative', left:'2rem'}}>
+                    <path   d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                
+                </svg>
+                
+        
+            <div className='cart-dropdown' style={{width:'370px', backgroundColor:'#5C2B29', borderRadius:'1rem',position:'relative',top:'2rem', width:'18rem'}}>
+            { openCart &&  
+    <div>
+
+    <span  onClick={clearCart} style={{paddingBottom:'15px'}}> <button style={{ borderRadius:'1rem', backgroundColor:'black', color:'yellow', border:'1px white solid'}}>  Vaciar carrito </button> </span>
+    <div> 
+        
+        
+
+
+        <>
+
+                    { openCart &&
+                        
+                        
+                        cart.map((item) => (
+                            <div style={{color:'white'}}>
+                {item.name} - {item.quantity} Unidades - Precio individual: {item.price}
+                <br />
+                -----------------------------------------
+                <br />
+            </div>
+        ))}
+            
+            
+            <div>
+                <br />
+                <span style={{color:'white'}}>Cantidad de Libros: {getTotalQuantity()}  </span>
+                <br />
+                <span style={{color:'white'}}>Precio Total: $ {getTotal()} </span>
+                <br />
+                <span> <button style={{ borderRadius:'1rem', backgroundColor:'black', color:'yellow', border:'1px white solid'}}> Ir a Pagar </button></span>
+                
+            </div> 
+                                </>
+         
+        
+    </div>
+    </div>  
+}
+            </div>
+            
+            
+    </div>
+
+                    
+</nav>
         
     )
 }
 
 export default NavBar
+
